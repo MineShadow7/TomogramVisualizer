c6726a28c613ecd095af91c74409b25297ebe3bd
Andrey Karagodin <shad-karagodin@ya.ru>
2023-04-17 10:28:29 +0300

    Добавьте файлы проекта.

---
+++ Карагодин_Tomogram_Visualizer/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Карагодин_Tomogram_Visualizer
+{
+    public partial class Form1 : Form
+    {
+        Bin bin = new Bin();
+        View view = new View();
+        bool loaded = false;
+        private int currentLayer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                string str = dialog.FileName;
+                bin.readBIN(str);
+                view.SetupView(glControl1.Width, glControl1.Height);
+                loaded = true;
+                glControl1.Invalidate();
+            }
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (loaded)
+            {
+                view.DrawQuads(currentLayer);
+                glControl1.SwapBuffers();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            currentLayer = trackBar1.Value;
+            
+        }
+    }
+}
