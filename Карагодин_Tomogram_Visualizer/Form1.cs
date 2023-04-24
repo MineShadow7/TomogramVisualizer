eeca96ef3d6d18fd21e4a57a91f8a5a29f428fc2
Andrey Karagodin <shad-karagodin@ya.ru>
2023-04-24 10:14:01 +0300

    Tomogramm Visualizer Done
    Сделано до 2-х переключателей рендера radiobutton

--- Карагодин_Tomogram_Visualizer/Form1.cs
+++ Карагодин_Tomogram_Visualizer/Form1.cs
@@ -16,6 +16,21 @@ namespace Карагодин_Tomogram_Visualizer
         View view = new View();
         bool loaded = false;
         private int currentLayer;
+        int FrameCount;
+        DateTime NextFPSUpdate = DateTime.Now.AddSeconds(1);
+        bool needReload = false;
+        bool renderType = false;
+        
+        void displayFPS()
+        {
+            if(DateTime.Now >= NextFPSUpdate)
+            {
+                this.Text = String.Format("CT Visualizer (fps={0})", FrameCount);
+                NextFPSUpdate = DateTime.Now.AddSeconds(1);
+                FrameCount = 0;
+            }
+            FrameCount++;
+        }
 
         public Form1()
         {
@@ -39,15 +54,59 @@ namespace Карагодин_Tomogram_Visualizer
         {
             if (loaded)
             {
-                view.DrawQuads(currentLayer);
-                glControl1.SwapBuffers();
+                if(renderType = false)
+                {
+                    view.DrawQuads(currentLayer);
+                    glControl1.SwapBuffers();
+                }
+                if (renderType = true)
+                {
+                    if (needReload)
+                    {
+                        view.generateTextureImage(currentLayer);
+                        view.Load2dTexture();
+                        needReload = false;
+                    }
+                    view.DrawTexture();
+                    glControl1.SwapBuffers();
+                }
             }
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
             currentLayer = trackBar1.Value;
-            
+            needReload = true;
+        }
+        
+        void Application_Idle(object sender, EventArgs e)
+        {
+            while (glControl1.IsIdle)
+            {
+                displayFPS();
+                glControl1.Invalidate();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.Idle += Application_Idle;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                renderType = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                renderType = true;
+            }
         }
     }
 }
